e1ea05c4a2420746f55d159b2c327f4193c83b5f
Damian Cervantes <dcervantes@ute.com.uy>
2020-07-23 14:06:43 -0300

    cambios

---
+++ js/partidos.js
@@ -0,0 +1,209 @@
+var fechas = [];
+var fecha1 = {no: '1ra', fecha: '07/07/2020', partidos: [
+    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
+    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
+    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'}
+]};
+fechas.push(fecha1);
+
+var fecha2 = {no: '2da', fecha: '08/07/2020', partidos: [
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
+    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'}
+]};
+fechas.push(fecha2);
+
+var fecha3 = {no: '3era', fecha: '08/07/2020', partidos: [
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
+    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'}
+]};
+fechas.push(fecha3);
+
+var fecha4 = {no: '4ta', fecha: '08/07/2020', partidos: [
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
+    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'}
+]};
+fechas.push(fecha4);
+
+var fecha5 = {no: '5ta', fecha: '08/07/2020', partidos: [
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
+    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'}
+]};
+fechas.push(fecha5);
+
+var fecha6 = {no: '6ta', fecha: '08/07/2020', partidos: [
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
+    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'}
+]};
+fechas.push(fecha6);
+
+var fecha7 = {no: '7ma', fecha: '08/07/2020', partidos: [
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
+    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'}
+]};
+fechas.push(fecha7);
+
+var fecha8 = {no: '8va', fecha: '08/07/2020', partidos: [
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
+    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'}
+]};
+fechas.push(fecha8);
+
+var fecha9 = {no: '9na', fecha: '08/07/2020', partidos: [
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
+    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'}
+]};
+fechas.push(fecha9);
+
+var fecha10 = {no: '10ma', fecha: '08/07/2020', partidos: [
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
+    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'}
+]};
+fechas.push(fecha10);
+
+var fecha11 = {no: '11ma', fecha: '08/07/2020', partidos: [
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
+    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
+]};
+fechas.push(fecha11);
+
+var fecha12 = {no: '12ma', fecha: '08/07/2020', partidos: [
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
+    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'}
+    
+]};
+fechas.push(fecha12);
+
+var fecha13 = {no: '13a', fecha: '08/07/2020', partidos: [
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
+    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+]};
+fechas.push(fecha13);
+
+var fecha14 = {no: '14a', fecha: '08/07/2020', partidos: [
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
+    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+]};
+fechas.push(fecha14);
+
+var fecha15 = {no: '15a', fecha: '08/07/2020', partidos: [
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
+    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+    {equipo1: 'Nacional', equipo2: 'Cuba'},
+    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+]};
+fechas.push(fecha15);
+
+
+var html = '';
+fechas.forEach(fecha => {
+    html += 
+    `
+    <div class="col-md-4"> 
+    <table>
+        <tr>
+            <td colspan="2" class="titulo">` + fecha.no + ` Fecha ` + fecha.fecha + `</td>
+        </tr>
+        <tr>
+        
+            <th>
+                Partido
+            </th>
+        </tr>`;
+    fecha.partidos.forEach(partido => {
+        html += `<tr>
+                
+                <td>
+                ` + partido.equipo1 + ` VS ` + partido.equipo2 + `
+                </td>
+            </tr>`;
+    });
+    html += `</table></div>`;
+});
+document.getElementById("tablas").innerHTML = html;
