f5c35c811770e582e70f423cc320d0bd6593487d
Damian Cervantes <dcervantes@ute.com.uy>
2020-07-24 13:53:48 -0300

    CAMBIOS

--- js/partidos.js
+++ js/partidos.js
@@ -71,112 +71,112 @@ var fecha6 = {no: '6ta', fecha: '23/08/2020', partidos: [
 ]};
 fechas.push(fecha6);
 
-var fecha7 = {no: '7ma', fecha: '08/07/2020', partidos: [
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
-    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
-    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'}
+var fecha7 = {no: '7ma', fecha: '30/08/2020', partidos: [
+    {equipo1: 'Deportivo Matadero', equipo2: 'Clara Jackson'},
+    {equipo1: 'Body Factory', equipo2: 'El Rejunte'},
+    {equipo1: 'Gomensoro', equipo2: 'El Chorro F.C'},
+    {equipo1: 'Sagrada familia', equipo2: 'Kennedy U.'},
+    {equipo1: 'Club Bigua V.B', equipo2: 'Tacuarembó'},
+    {equipo1: 'Ingles', equipo2: 'Old Sampa'},
+    {equipo1: 'Esparta U.', equipo2: 'Flores U.'},
+    {equipo1: 'San Eugenio', equipo2: 'San Agustin'}
 ]};
 fechas.push(fecha7);
 
-var fecha8 = {no: '8va', fecha: '08/07/2020', partidos: [
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
-    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
-    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'}
+var fecha8 = {no: '8va', fecha: '06/09/2020', partidos: [
+    {equipo1: 'Clara Jackson', equipo2: 'Club Bigua V.B'},
+    {equipo1: 'Flores U.', equipo2: 'San Eugenio'},
+    {equipo1: 'Old Sampa', equipo2: 'Esparta U.'},
+    {equipo1: 'Tacuarembó', equipo2: 'Ingles'},
+    {equipo1: 'San Agustin', equipo2: 'Sagrada Familia'},
+    {equipo1: 'El Chorro F.C', equipo2: 'Deportivo Matadero'},
+    {equipo1: 'El Rejunte', equipo2: 'Gomensoro'},
+    {equipo1: 'Kennedy U.', equipo2: 'Body Factory'}
 ]};
 fechas.push(fecha8);
 
-var fecha9 = {no: '9na', fecha: '08/07/2020', partidos: [
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
-    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
-    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'}
+var fecha9 = {no: '9na', fecha: '13/09/2020', partidos: [
+    {equipo1: 'Ingles', equipo2: 'Clara Jackson'},
+    {equipo1: 'Gomensoro', equipo2: 'Kennedy U.'},
+    {equipo1: 'Deportivo Matadero', equipo2: 'El Rejunte'},
+    {equipo1: 'Club Bigua V.B', equipo2: 'El Chorro F.C'},
+    {equipo1: 'Sagrada Familia', equipo2: 'Body Factory'},
+    {equipo1: 'Esparta U.', equipo2: 'Tacuarembó'},
+    {equipo1: 'San Eugenio', equipo2: 'Old Sampa'},
+    {equipo1: 'San Agustin', equipo2: 'Flores U.'}
 ]};
 fechas.push(fecha9);
 
-var fecha10 = {no: '10ma', fecha: '08/07/2020', partidos: [
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
-    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
-    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'}
+var fecha10 = {no: '10ma', fecha: '20/09/2020', partidos: [
+    {equipo1: 'Clara Jackson', equipo2: 'Esparta U.'},
+    {equipo1: 'Old Sampa', equipo2: 'San Agustin'},
+    {equipo1: 'Tacuarembó', equipo2: 'San Eugenio'},
+    {equipo1: 'Flores U.', equipo2: 'Sagrada Familia'},
+    {equipo1: 'El Chorro F.C', equipo2: 'Ingles'},
+    {equipo1: 'El Rejunte', equipo2: 'Club Bigua V.B'},
+    {equipo1: 'Kennedy U.', equipo2: 'Deportivo Matadero'},
+    {equipo1: 'Body Factory', equipo2: 'Gomensoro'}
 ]};
 fechas.push(fecha10);
 
-var fecha11 = {no: '11ma', fecha: '08/07/2020', partidos: [
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
-    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
-    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
-    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
+var fecha11 = {no: '11ma', fecha: '04/10/2020', partidos: [
+    {equipo1: 'San Eugenio', equipo2: 'Clara Jackson'},
+    {equipo1: 'Deportivo Matadero', equipo2: 'Body Factory'},
+    {equipo1: 'Club Bigua V.B', equipo2: 'Kennedy U.'},
+    {equipo1: 'Ingles', equipo2: 'El Rejunte'},
+    {equipo1: 'Esparta U.', equipo2: 'El Chorro F.C'},
+    {equipo1: 'Sagrada Familia', equipo2: 'Gomensoro'},
+    {equipo1: 'San Agustin', equipo2: 'Tacuarembó'},
+    {equipo1: 'Flores U.', equipo2: 'Old Sampa'},
 ]};
 fechas.push(fecha11);
 
-var fecha12 = {no: '12ma', fecha: '08/07/2020', partidos: [
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
-    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
-    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'}
+var fecha12 = {no: '12ma', fecha: '11/10/2020', partidos: [
+    {equipo1: 'Clara Jackson', equipo2: 'San Agustin'},
+    {equipo1: 'Tacuarembó', equipo2: 'Flores U.'},
+    {equipo1: 'Old Sampa', equipo2: 'Sagrada Familia'},
+    {equipo1: 'El Chorro F.C', equipo2: 'San Eugenio'},
+    {equipo1: 'El Rejunte', equipo2: 'Esparta U.'},
+    {equipo1: 'Kennedy U.', equipo2: 'Ingles'},
+    {equipo1: 'Body Factory', equipo2: 'Club Bigua V.B'},
+    {equipo1: 'Gomensoro', equipo2: 'Deportivo Matadero'}
     
 ]};
 fechas.push(fecha12);
 
-var fecha13 = {no: '13a', fecha: '08/07/2020', partidos: [
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
-    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
-    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+var fecha13 = {no: '13a', fecha: '18/07/2020', partidos: [
+    {equipo1: 'Flores U.', equipo2: 'Clara Jackson'},
+    {equipo1: 'Club Bigua V.B', equipo2: 'Gomensoro'},
+    {equipo1: 'Ingles', equipo2: 'Body Factory'},
+    {equipo1: 'Esparta U.', equipo2: 'Kennedy U.'},
+    {equipo1: 'San Eugenio', equipo2: 'El Rejunte'},
+    {equipo1: 'San Agustin', equipo2: 'El Chorro F.C'},
+    {equipo1: 'Sagrada Familia', equipo2: 'Deportivo Matadero'},
+    {equipo1: 'Old Sampa', equipo2: 'Tacuarembó'},
 ]};
 fechas.push(fecha13);
 
-var fecha14 = {no: '14a', fecha: '08/07/2020', partidos: [
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
-    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
-    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+var fecha14 = {no: '14a', fecha: '25/10/2020', partidos: [
+    {equipo1: 'Clara Jackson', equipo2: 'Old Sampa'},
+    {equipo1: 'Tacuarembó', equipo2: 'Sagrada Familia'},
+    {equipo1: 'El Chorro F.C', equipo2: 'Flores U.'},
+    {equipo1: 'El Rejunte', equipo2: 'San Agustin'},
+    {equipo1: 'Kennedy U.', equipo2: 'San Eugenio'},
+    {equipo1: 'Body Factory', equipo2: 'Esparta U.'},
+    {equipo1: 'Gomensoro', equipo2: 'Ingles'},
+    {equipo1: 'Deportivo Matadero', equipo2: 'Club Bigua V.B'},
 ]};
 fechas.push(fecha14);
 
-var fecha15 = {no: '15a', fecha: '08/07/2020', partidos: [
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
-    {equipo1: 'Equipo 1', equipo2: 'Equipo 2'},
-    {equipo1: 'Equipo 4', equipo2: 'Equipo 4'},
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
-    {equipo1: 'Nacional', equipo2: 'Cuba'},
-    {equipo1: 'Fulano', equipo2: 'Fulanito'},
+var fecha15 = {no: '15a', fecha: '01/11/2020', partidos: [
+    {equipo1: 'Tacuarembó', equipo2: 'Clara Jackson'},
+    {equipo1: 'Ingles', equipo2: 'Deportivo Matadero'},
+    {equipo1: 'Esparta U.', equipo2: 'Gomensoro'},
+    {equipo1: 'San Eugenio', equipo2: 'Body Factory'},
+    {equipo1: 'San Agustin', equipo2: 'Kennedy U.'},
+    {equipo1: 'Flores U.', equipo2: 'El Rejunte'},
+    {equipo1: 'Old Sampa', equipo2: 'El Chorro F.C'},
+    {equipo1: 'Sagrada Familia', equipo2: 'Club Bigua V.B'},
 ]};
 fechas.push(fecha15);
 
